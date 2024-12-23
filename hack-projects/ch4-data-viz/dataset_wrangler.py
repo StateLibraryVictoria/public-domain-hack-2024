375ad57f2ea12c274be19a7ad738b0711016dff1
Sotirios Alpanis <SAlpanis@slv.vic.gov.au>
2024-12-23 16:55:03 +1100

    Removed unsupported tools

--- hack-projects/ch4-data-viz/dataset_wrangler.py
+++ hack-projects/ch4-data-viz/dataset_wrangler.py
@@ -85,7 +85,8 @@ def create_grid():
     x = [coord[0] for coord in coords]
     y = [coord[1] for coord in coords]
 
-    TOOLS = "hover,crosshair,pan,wheel_zoom,zoom_in,zoom_out,box_zoom,undo,redo,reset,tap,save,box_select,poly_select,lasso_select,examine,help"
+    # TOOLS = "hover,crosshair,pan,wheel_zoom,zoom_in,zoom_out,box_zoom,undo,redo,reset,tap,save,box_select,poly_select,lasso_select,examine,help"
+    TOOLS = "hover,crosshair,pan,wheel_zoom,zoom_in,zoom_out,box_zoom,undo,redo,reset"
 
     p = figure(tools=TOOLS)
 
